c2710e2e11a920db8cb80561c7aaf5fb74b1e7b2
Andrew Smith <topcodeandrew@gmail.com>
2020-11-09 19:28:56 -0700

    mid way commit

--- higher-order.js
+++ higher-order.js
@@ -67,6 +67,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
   (runningTotal, curElement, curIndex, wholeArray)=>{} Arrow Form
 */
 
+let totalPopulation = populations.reduce((acc, element) => { return acc + element})
 
 
 ////////// PROBLEM 4 //////////
@@ -91,7 +92,12 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter((element, key) => {
+  if (element.CP > 200){
+    return element;
+  }
+}
+)
 
 
 
@@ -105,10 +111,11 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 // Do not edit code above.
 
 /*
-  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
+  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, 
+    hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map (element => element.price + (element.price * element.tax))
 
 
 
@@ -128,6 +135,12 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.reduce((acc, element, index) => {
+  if(element.owner === "Bob") {
+    return acc + element.price
+  } else {
+    return acc
+  }
+}, 0)
 
 
